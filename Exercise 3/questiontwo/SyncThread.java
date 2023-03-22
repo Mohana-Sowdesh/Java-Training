f6c47bebecf76b56885499c2bcaae13759689ce9
Mohana-Sowdesh <mohanasowdesh.ramadoss@cdw.com>
2023-03-22 11:18:42 +0530

    Revert "made changes in class & method naming"
    This reverts commit 72f7219a2f5f8a889d220d45f4956ff1dd610e6c.

--- Exercise 3/questiontwo/SyncThread.java
+++ Exercise 3/questiontwo/SyncThread.java
@@ -4,7 +4,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class SyncThread {
-<<<<<<< Updated upstream
 	 int i = 0;
 	 public static void main(String[] args) {
 		
@@ -13,34 +12,16 @@ public class SyncThread {
 		ExecutorService executorService = Executors.newFixedThreadPool(1);
 		
 		for(int j = 0; j < 5; j++) {
-=======
-	int i = 0;
-
-	public static void main(String[] args) {
-
-		SyncThread syncThread = new SyncThread();
-
-		ExecutorService executorService = Executors.newFixedThreadPool(1);
-
-		for (int j = 0; j < 5; j++) {
->>>>>>> Stashed changes
 			executorService.execute(() -> {
 				syncThread.printMessage();
 			});
 		}
 		executorService.shutdown();
 	}
-<<<<<<< Updated upstream
 	 
 	public void printMessage() {
 		String msgArray[] = {"Chennai", "Mumbai", "Banglore", "Pune", "Hyderabad"};
 		System.out.println("-------"+msgArray[i]+"------");
-=======
-
-	public void printMessage() {
-		String msgArray[] = { "Chennai", "Mumbai", "Banglore", "Pune", "Hyderabad" };
-		System.out.println("-------" + msgArray[i] + "------");
->>>>>>> Stashed changes
 		i++;
 	}
 }

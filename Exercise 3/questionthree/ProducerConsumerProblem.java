57d008349ab5e6a83bd150f2300fc2105471072a
Mohana-Sowdesh <mohanasowdesh.ramadoss@cdw.com>
2023-03-22 11:18:58 +0530

    Revert "Revert "made changes in class & method naming""
    This reverts commit f6c47bebecf76b56885499c2bcaae13759689ce9.

--- Exercise 3/questionthree/ProducerConsumerProblem.java
+++ Exercise 3/questionthree/ProducerConsumerProblem.java
@@ -4,6 +4,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class ProducerConsumerProblem {
+<<<<<<< Updated upstream
 	
 	public static void main(String[] args) {
 		Inventory inventory = new Inventory();
@@ -21,6 +22,25 @@ public class ProducerConsumerProblem {
 				inventory.consumeProduct();
 			}
 		});	
+=======
+
+	public static void main(String[] args) {
+		Inventory inventory = new Inventory();
+
+		ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+		executorService.execute(() -> {
+			for (int j = 0; j < 3; j++) {
+				inventory.produceProduct();
+			}
+		});
+
+		executorService.execute(() -> {
+			for (int j = 0; j < 3; j++) {
+				inventory.consumeProduct();
+			}
+		});
+>>>>>>> Stashed changes
 		executorService.shutdown();
 	}
 
@@ -28,12 +48,21 @@ public class ProducerConsumerProblem {
 
 class Inventory {
 	int productState = 0;
+<<<<<<< Updated upstream
 	synchronized public void produceProduct() {
 		if(productState == 1) {
 			try {
 				wait();
 			}
 			catch(Exception exception) {
+=======
+
+	synchronized public void produceProduct() {
+		if (productState == 1) {
+			try {
+				wait();
+			} catch (Exception exception) {
+>>>>>>> Stashed changes
 				exception.printStackTrace();
 			}
 		}
@@ -43,11 +72,18 @@ class Inventory {
 	}
 
 	synchronized public void consumeProduct() {
+<<<<<<< Updated upstream
 		if(productState == 0) {
 			try {
 				wait();
 			}
 			catch(Exception exception) {
+=======
+		if (productState == 0) {
+			try {
+				wait();
+			} catch (Exception exception) {
+>>>>>>> Stashed changes
 				exception.printStackTrace();
 			}
 		}

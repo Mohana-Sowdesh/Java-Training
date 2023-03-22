4bb3dd4812409843b5e2bfd80c0d42b539c5838e
Mohana-Sowdesh <mohanasowdesh.ramadoss@cdw.com>
2023-03-22 15:03:42 +0530

    worked on exercise 3

---
+++ Exercise 3/questionthree/questionfive/ThreadStartTwice.java
@@ -0,0 +1,21 @@
+package questionfive;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ThreadStartTwice {
+
+	public static void main(String[] args) {
+
+		Thread thread1 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("Thread started....");
+			}
+		});
+
+		thread1.start();
+		thread1.start();
+	}
+
+}
